0d4b46b2f055e33ea71cb43a12d5a2496e6eee5a
Konstantin <konstantin.zurov@gmail.com>
2022-07-04 20:58:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`http://someurl/${category}`);
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`);
+
+
+
+/////////////////////////////////////////
+//let number = 5;
+//const leftBorderWidth = 1;
+
+//number = 10;
+//console.log (number);
+////////////////////////////////////
+//const obj = {
+//    name: "Zurquat",
+ //   age: 29,
+ //   isMarried: false
+//};
+
+//console.log(obj["name"]);
+////////////////////////////////////////
+//let arr  = ["plumb.png", "yanke.jpg", "ioqp.bpm", 589, {name: "Anka", age: 27}];
+
+//console.log(arr[2]);
+//////////////////////////////////////////
+
+//const arrObj = {
+//    0: "a",
+ //   1: "b",
+ //   2: 'c'
+//}
+
+//arrObj.b = [1234];
+
+
+//console.log(arrObj['b']);
+
+////////////////////////////////////
+
+//alert('Hello');
+
+//const result = confirm("How are you?");
+//console.log (result);
+
+//const answer = prompt("вам есть 18?","18");
+//console.log(typeof(answer));
+
+//const answers = [];
+
+//answers[0] = prompt('Kak Dela?','');
+//answers[1] = prompt('ewe HE PoDuJIa?','');
+//answers[2] = prompt('Cam Kak?','');
+
+//console.log(typeof(answers));
